e7cd827a984d33f17ced539bc2690a3a2ae59c90
Facundo Fernandez <ff96_@hotmail.com>
2023-01-26 13:18:48 -0300

    form login & api token

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import swal from "@sweetalert/with-react";
+
+export const Login = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (email === "" || password === "") {
+      swal(<h2>Los campos no pueden estar vacios</h2>);
+      return;
+    }
+
+    if (email !== "" && !regexEmail.test(email)) {
+      swal(<h2>Debes escribir un email válido</h2>);
+      return;
+    }
+
+    if (email !== "challenge@alkemy.org" || password !== "react") {
+      swal(<h2>Credenciales inválidas</h2>);
+      return;
+    }
+
+    axios
+      .post("http://challenge-react.alkemy.org", { email, password })
+      .then((res) => {
+        swal(<h2>Estás dentro!</h2>);
+        console.log(res.data);
+      });
+  };
+
+  return (
+    <>
+      <h2>Formulario de Login</h2>
+      <form onSubmit={submitHandler}>
+        <label>
+          <span>Correo Electronico:</span> <br />
+          <input type="text" name="email" />
+        </label>
+        <br />
+        <label>
+          <span>Contraseña:</span> <br />
+          <input type="password" name="password" />
+        </label>
+        <br />
+        <button type="submit">Ingresar</button>
+      </form>
+    </>
+  );
+};
